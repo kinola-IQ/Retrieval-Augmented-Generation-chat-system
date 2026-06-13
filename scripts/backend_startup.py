8c5ce28a189f3ac325d688ce0af7131a11f13034
akinola <akdunmininu@gmail.com>
2026-06-13 01:56:40 +0100

    tweaked backend

--- scripts/backend_startup.py
+++ scripts/backend_startup.py
@@ -10,8 +10,8 @@ from core.utils.logger import logger
 
 load_dotenv()
 # api configurations
-host = os.environ["HOST"]
-port = os.environ["PORT"]
+host = os.environ.get("HOST","0.0.0.0")
+port = os.environ.get("PORT", 8000)
 
 
 # backend startup
